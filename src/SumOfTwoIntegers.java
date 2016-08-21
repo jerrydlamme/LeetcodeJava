55c93984079f248618ac6b042b5ce62ee311ffdd
Xinyu Zhao <jerrydlamme@gmail.com>
2016-08-21 00:15:51 -0700

    129

--- src/SumOfTwoIntegers.java
+++ src/SumOfTwoIntegers.java
@@ -1,4 +1,15 @@
 
 public class SumOfTwoIntegers {
-
+    public int getSum(int a, int b) {
+        
+        while ((a & b) != 0) {
+            int x = (a & b) << 1;
+            int y = a ^ b;
+            
+            a = x;
+            b = y;
+        }
+        
+        return a | b;
+    }
 }
